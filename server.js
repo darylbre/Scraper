2625e68b117413dcbdfb87440585426c4917c357
darylbre <roderickbreland@gmail.com>
2019-06-24 20:46:47 -0400

    changes to server.js

--- server.js
+++ server.js
@@ -2,13 +2,9 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 
 var PORT = 3000;
@@ -27,30 +23,27 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/unit18Populater", {
+  useNewUrlParser: true
+});
 
-// Routes
-
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get('/scrape', function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.mtonews.com").then(function(response) {
+  axios.get('http://www.mtonews.com').then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
-    // console.log(response.data);
+    console.log(response.data);
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every m-card-group within an article tag, and do the following:
-    $(".m-card-group").each(function(i, element) {
-      console.log('element', element);
-
+    // Now, we grab every h2 within an article tag, and do the following:
+    $('h2').each(function(i, element) {
       // Save an empty result object
       var result = {};
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).find("a").find('h2')
-        .text();
-      result.link = $(this).find("a")
-        .attr("href");
-        console.log('result: ', result);   
+      result.title = $(this).text();
+      result.link = $(this)
+        .parent()
+        .attr('href');
+      console.log('result: ', result);
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
@@ -63,9 +56,8 @@ app.get("/scrape", function(req, res) {
           console.log(err);
         });
     });
-
     // Send a message to the client
-    res.send("Scrape Complete");
+    res.send('Scrape Complete');
   });
 });
 
@@ -104,10 +96,11 @@ app.post("/articles/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
